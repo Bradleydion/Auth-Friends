6a1776871fa71be48e40eadd08259b6d392677f4
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-01-17 17:55:25 -0500

    I belive my forms are complete

--- friends/src/components/addFriend.js
+++ friends/src/components/addFriend.js
@@ -1,23 +1,38 @@
 import React from "react"
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
 const AddFriend = () => {
 
     return(
-        <div className="AddFriendCard">
-            <input
-            type="text"
-            name="fullname"
-            placeholder="Full Name"/>
-            <input
-            type="text"
-            name="age"
-            placeholder="Age"/>
-            <input
-            type="text"
-            name="email"
-            placeholder="Email"/>
-            <button name="addFriend">Add Friend</button>
-        </div>
-    )
+        // <div className="AddFriendCard">
+        //     <input
+        //     type="text"
+        //     name="fullname"
+        //     placeholder="Full Name"/>
+        //     <input
+        //     type="text"
+        //     name="age"
+        //     placeholder="Age"/>
+        //     <input
+        //     type="text"
+        //     name="email"
+        //     placeholder="Email"/>
+        //     <button name="addFriend">Add Friend</button>
+        // </div>
+
+        <Form>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label for="fullName" className="mr-sm-2">Friends Full Name</Label>
+          <Input type="text" name="fullName" id="fullName" placeholder="Friends Full Name" />
+        </FormGroup>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label for="age" className="mr-sm-2">Age</Label>
+          <Input type="age" name="age" id="age" placeholder="Age" />
+          <Label for="exampleEmail" className="mr-sm-2">Email</Label>
+        <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+        </FormGroup>
+        <Button>Add Friend</Button>
+      </Form>
+    );
 }
 export default AddFriend
--- friends/src/components/login.js
+++ friends/src/components/login.js
@@ -6,11 +6,11 @@ const Login = () => {
     return (
           <Form inline>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Label for="exampleEmail" className="mr-sm-2">Email</Label><br></br>
+        <Label for="exampleEmail" className="mr-sm-2">Email</Label>
         <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
       </FormGroup>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Label for="examplePassword" className="mr-sm-2">Password</Label><br></br>
+        <Label for="examplePassword" className="mr-sm-2">Password</Label>
         <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
       </FormGroup>
       <Button>Submit</Button>
