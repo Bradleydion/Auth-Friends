1bbef9cf24b96c375b2989e3aefe3287c85d6cf9
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-01-18 20:05:15 -0500

    minus styling, all of the mvp is met

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -12,32 +12,45 @@ class Friends extends React.Component {
     componentDidMount() {
         this.getData();
     }
-
+  
     getData = () => {
         axiosWithAuth().get("/api/friends")
         .then(
             res => {
                 console.log(res);
                 this.setState({
-                    friendList:res.data.data
+                    friendList:res.data
                 })
-                
             }
         )
         .catch(err => console.log(err))
     }
+//   delete= (e) =>{
+//       console.log(e)
+//     axiosWithAuth().delete(`/api/friends/${e}`)
+//     .then(
+//         res =>{
+//             console.log(res)
+//         }
+        
+//     )
+      
+//   }
 
     render(){
+        console.log(this.state.friendList)
   return (
     <div>
-      <Card>
+        {this.state.friendList.map(friend =>
+        <Card key={friend.id}>
         <CardBody>
-          <CardTitle tag="h5">My Friend: </CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Age:</CardSubtitle>
-          <CardText>Email Address:</CardText>
-          <Button>Delete Friend</Button>
+          <CardTitle tag="h5">My Friend:{friend.name}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Age:{friend.age}</CardSubtitle>
+          <CardText>Email Address:{friend.email}</CardText>
+          <Button >Delete Friend</Button>
         </CardBody>
-      </Card>
+      </Card>)}
+      
     </div>
   )};
 };
--- friends/src/components/addFriend.js
+++ friends/src/components/addFriend.js
@@ -1,38 +1,53 @@
 import React from "react"
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
-
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 class AddFriend extends React.Component{
+
+  state ={
+      newFriend:{
+            id:Date.now,
+            name:'',
+            age:'',
+            email:''
+        }
+    }
+    handleChange=(e)=>{
+        this.setState({
+            newFriend:{
+                ...this.state.newFriend,
+                [e.target.name]:e.target.value}
+            })
+
+    }
+    addFriend= (e) =>{
+        e.preventDefault()
+        axiosWithAuth().post('/api/friends', this.state.newFriend)
+        .then(
+            res => {
+                console.log(res)
+            }
+
+        )
+        .catch(err=>{
+            console.log(err)
+        }
+
+        )}
 
     render(){
     return(
-        // <div className="AddFriendCard">
-        //     <input
-        //     type="text"
-        //     name="fullname"
-        //     placeholder="Full Name"/>
-        //     <input
-        //     type="text"
-        //     name="age"
-        //     placeholder="Age"/>
-        //     <input
-        //     type="text"
-        //     name="email"
-        //     placeholder="Email"/>
-        //     <button name="addFriend">Add Friend</button>
-        // </div>
-
-        <Form>
+        <Form >
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="fullName" className="mr-sm-2">Friends Full Name</Label>
-          <Input type="text" name="fullName" id="fullName" placeholder="Friends Full Name" />
+          <Input onChange ={this.handleChange} value={this.state.newFriend.name}type="text" name="name" id="fullName" placeholder="Friends Full Name" />
         </FormGroup>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="age" className="mr-sm-2">Age</Label>
-          <Input type="age" name="age" id="age" placeholder="Age" />
+          <Input onChange ={this.handleChange} value={this.state.newFriend.age} type="text" name="age" id="age" placeholder="Age" />
           <Label for="exampleEmail" className="mr-sm-2">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+        <Input onChange ={this.handleChange} value={this.state.newFriend.email}type="email" name="email" id="exampleEmail" placeholder="something@idk.cool"/>
         </FormGroup>
-        <Button>Add Friend</Button>
+        <Button onClick={this.addFriend}>Add Friend</Button>
       </Form>
     );}
 }
